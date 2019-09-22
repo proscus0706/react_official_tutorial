4f5d8c5af1724598df3329f4c93477262d98e582
shin <proscus0706@gmail.com>
2019-09-22 17:26:13 +0900

    last commit

---
+++ my-app2/my-app/src/components/RefsDemo.js
@@ -0,0 +1,11 @@
+import React, {Component} from './node_modules/react'
+
+class RefsDemo extends Component {
+    render(){
+        return (
+            <div>
+                <input type="text"/>
+            </div>
+        )
+    }
+}
